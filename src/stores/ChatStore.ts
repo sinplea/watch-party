c084559e2752d82a4d0c48026944ae82dba1f60d
Alex Werner <alexanderdwerner@gmail.com>
2020-10-16 15:23:00 -0500

    Create svelte store for chat composite component

---
+++ src/stores/ChatStore.ts
@@ -0,0 +1,41 @@
+/**
+ * src/stores/ChatStore.js
+ * 
+ * Handles any global storage needs for the main Chat Panel component.
+ * Currently creates Svelte store(s) for managing:
+ * 
+ * Global Sender (see src/stories/chat/Interfaces)
+ */
+import { writable } from 'svelte/store';
+import type { Sender } from '../stories/Chat/Interfaces';
+
+// Default Values
+
+let defaultSender: Sender = {
+    name: 'DefaultValue',
+    color: '#ffffff',
+};
+
+// Custom Stores
+// 
+// NOTE: The functions changeName & changeColor are maybe a bit extrenuous
+// becuase I could just use the native update function inside components that use the store,
+// but I'm trying to learn more about custom stores in this example as that seems important
+// based on work done with libraries like Vuex
+function createSender() {
+    const { subscribe, set, update } = writable(defaultSender);
+
+    return {
+        subscribe,
+        set,
+        changeName: (name: string): void => {
+            update((sender: Sender) => ({...sender, name }));
+        },
+        changeColor: (color: string): void => {
+            update((sender: Sender) => ({ ...sender, color }));
+        },
+    };
+};
+
+// Store exports
+export const sender = createSender();
