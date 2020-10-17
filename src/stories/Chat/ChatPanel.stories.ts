738bfd7d08370757babd9a74f3c02ebc9afc0f9e
Alex Werner <alexanderdwerner@gmail.com>
2020-10-17 00:44:18 -0500

    adds chat panel composite

---
+++ src/stories/Chat/ChatPanel.stories.ts
@@ -0,0 +1,10 @@
+import ChatPanel from './ChatPanel.svelte';
+
+export default {
+    title: 'Chat/Panel',
+    component: ChatPanel,
+};
+
+export const Empty = () => ({
+    Component: ChatPanel,
+});
---
+++ src/stories/Chat/ChatPanel.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import ChatMessageList from './ChatMessageList.svelte';
+    import ChatInput from './ChatInput.svelte';
+    import { sender } from '../../stores/ChatStore';
+    import type { Message } from './Interfaces';
+
+    export let messages: Message[] = [];
+
+    function postMessage(e: CustomEvent<any>) {
+        const message: Message = e.detail.message;
+        messages = [...messages, message];
+    };
+</script>
+
+<div class="chat-panel">
+    <div class="top-bar">Chat Room</div>
+    <ChatMessageList messages={messages}></ChatMessageList>
+    <ChatInput sender={$sender} on:onMessageSent={postMessage}></ChatInput>
+</div>
