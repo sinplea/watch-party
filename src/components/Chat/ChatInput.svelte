61a756a3c63eccf54b5b5db361baec8e58b1e34c
Alex Werner <alexanderdwerner@gmail.com>
2020-10-22 01:38:04 -0500

    restructures project to better seperate concerns e.g component files vs storybook files

---
+++ src/components/Chat/ChatInput.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+    import { createEventDispatcher } from 'svelte';
+    import type { Message, Sender } from '../../lib/ChatInterfaces';
+
+    const dispatch = createEventDispatcher();
+
+    export let sender: Sender = {
+        name: 'DefaultValue',
+        color: '#f1f1f1',
+    };
+
+    let messageValue: string = "";
+    
+    function onKeydown(e: KeyboardEvent) {
+        const key: string = e.code;
+
+        const message: Message = {
+            sender,
+            body: messageValue,
+            created_at: new Date(),
+        };
+
+        if (key === 'Enter') {
+            e.preventDefault();
+            
+            if (messageValue !== '') {
+                // Custom Event to pass along message
+                dispatch('onMessageSent', {
+                    message,
+                });
+
+                // Reset text area
+                messageValue = ''
+            }
+        };
+    };
+</script>
+
+<div class="chat-input-wrapper">
+    <textarea 
+        name="chat-input" 
+        id="chat-input" 
+        placeholder="Send a message"
+        bind:value={messageValue}
+        on:keydown={onKeydown}></textarea>
+
+</div>
+
+
+<style>
+    .chat-input-wrapper {
+        grid-column-start: 1;
+        grid-column-end: 2;
+        grid-row-start: 3;
+        grid-row-end: 4;
+        align-self: end;
+        padding: 0 4%;
+        padding-top: 4%;
+        margin-bottom: 2%;
+        border-top: #eee 2px solid;
+    }
+
+    textarea {
+        height: 40px;
+        width: 100%;
+        resize: none;
+        -webkit-border-radius: 5px;
+        -moz-border-radius: 5px;
+        border-radius: 5px;
+        font-family: 'Mukta', sans-serif;
+        font-weight: lighter;
+        padding-top: 10px;
+        box-sizing: border-box;
+    }
+</style>
---
+++ src/components/Chat/ChatPanel.svelte
@@ -0,0 +1,57 @@
+<svelte:head>
+    <link href="https://fonts.googleapis.com/css2?family=Mukta&display=swap" rel="stylesheet">
+</svelte:head>
+
+<script lang="ts">
+    import ChatMessageList from './ChatMessageList.svelte';
+    import ChatInput from './ChatInput.svelte';
+    import { sender } from '../../stores/ChatStore';
+    import type { Message } from '../../lib/ChatInterfaces';
+
+    export let messages: Message[] = [];
+
+    function postMessage(e: CustomEvent<any>) {
+        const message: Message = e.detail.message;
+        messages = [...messages, message];
+    };
+</script>
+
+<div class="chat-panel">
+        <h2 class="top-bar">CHAT ROOM</h2>
+        <div class="seperator"></div>
+        <ChatMessageList messages={messages}></ChatMessageList>
+
+        <ChatInput sender={$sender} on:onMessageSent={postMessage}></ChatInput>
+</div>
+
+<style>
+    .chat-panel {
+        display: grid;
+        width: 30%;
+        min-height: 90vh;
+        max-height: 90vh;
+        grid-template-columns: minmax(0, 90vh);
+        grid-template-rows: 6% auto 12%;
+        border-left: 2px solid #eee;
+    }
+
+    .top-bar {
+        font-family: 'Mukta', sans-serif;
+        font-size: 18px;
+        text-align: center;
+        align-self: center;
+        grid-column-start: 1;
+        grid-column-end: 2;
+        grid-row-start: 1;
+        grid-row-end: 2;
+    }
+
+    .seperator {
+        grid-column-start: 1;
+        grid-column-end: 2;
+        grid-row-start: 1;
+        grid-row-end: 2;
+        align-self: end;
+        border-bottom: 2px solid #eee;
+    }
+</style>
