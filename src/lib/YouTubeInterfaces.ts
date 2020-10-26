8914f2f5b4071200e6c623f4a83eb06508a1c0dd
Alex Werner <alexanderdwerner@gmail.com>
2020-10-26 09:24:16 -0500

    create YouTube API specific interfaces for response handling based on example json files

---
+++ src/lib/YouTubeInterfaces.ts
@@ -0,0 +1,75 @@
+// Interfaces for handling data coming from the YouTube API
+// Specifically handles data coming from /search & /videos listings
+// https://developers.google.com/youtube/v3/docs/search/list
+// https://developers.google.com/youtube/v3/docs/videos/list
+
+export interface YouTubeSearchResponse {
+    kind: string,
+    etag: string,
+    nextPageToken: string,
+    regionCode: string,
+    pageInfo: {
+        totalResults: number,
+        resultsPerPage: number,
+    },
+    items: [YouTubeSearchResponseItem],
+};
+
+export interface YouTubeSearchResponseItem {
+    kind: string,
+    etag: string,
+    id: {
+        kind: string,
+        videoId: string,
+    },
+    snippet: {
+        publishedAt: string,
+        channelId: string,
+        title: string,
+        description: string,
+        thumbnails: {
+            default: {
+                url: string,
+                width: number,
+                height: number,
+            },
+            medium: {
+                url: string,
+                width: number,
+                height: number,
+            },
+            high: {
+                url: string,
+                width: number,
+                height: number,
+
+            },
+        },
+        channelTitle: string,
+        liveBroadcastContent: string,
+        publishTime: string,
+    }
+};
+
+export interface YoutubeVideoStatisticsResponse {
+    kind: string,
+    etag: string,
+    items: [YoutubeVideoStatisticsResponseItem],
+    pageInfo: {
+        totalResults: number,
+        resultsPerPage: number,
+    },
+};
+
+export interface YoutubeVideoStatisticsResponseItem {
+    kind: string,
+    etag: string,
+    id: string,
+    statistics: {
+        viewCount: string,
+        likeCount: string,
+        dislikeCount: string,
+        favoriteCount: string,
+        commentCount: string,
+    },
+};
