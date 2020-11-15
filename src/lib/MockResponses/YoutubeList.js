4e72514def6b2095fa1b149c559c9f42d42ecef3
Alex Werner <alexanderdwerner@gmail.com>
2020-11-15 07:40:10 -0600

    Mocks YouTube responses as javascript

---
+++ src/lib/MockResponses/YoutubeList.js
@@ -0,0 +1,119 @@
+export default {
+  kind: "youtube#searchListResponse",
+  etag: "RSN5XP2KYb8JMYWZKOT2kvc8tB8",
+  nextPageToken: "CAMQAA",
+  regionCode: "US",
+  pageInfo: {
+    totalResults: 1000000,
+    resultsPerPage: 3,
+  },
+  items: [
+    {
+      kind: "youtube#searchResult",
+      etag: "fVjFF4q8llNwBSRpldClmIyaPBA",
+      id: {
+        kind: "youtube#video",
+        videoId: "MUCN-JwUvbY",
+      },
+      snippet: {
+        publishedAt: "2020-09-22T16:00:03Z",
+        channelId: "UC6pGDc4bFGD1_36IKv3FnYg",
+        title: "Attack on Titan Final Season | OFFICIAL TRAILER",
+        description:
+          "Watch Attack on Titan on Crunchyroll: https://got.cr/Watch-Titan Crunchyroll Collection brings you the latest clips, OPs, and more from your favorite anime!",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/MUCN-JwUvbY/default.jpg",
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/MUCN-JwUvbY/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/MUCN-JwUvbY/hqdefault.jpg",
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: "Crunchyroll Collection",
+        liveBroadcastContent: "none",
+        publishTime: "2020-09-22T16:00:03Z",
+      },
+    },
+    {
+      kind: "youtube#searchResult",
+      etag: "0SdILcS8S2C0vO6hmAcQUOAhgsw",
+      id: {
+        kind: "youtube#video",
+        videoId: "dZ_IwkAE2ls",
+      },
+      snippet: {
+        publishedAt: "2019-12-14T16:00:11Z",
+        channelId: "UC-pE29OUR43BqqBxIeyo_GA",
+        title:
+          "The Complete Attack on Titan Timeline...So Far  | Channel Frederator",
+        description:
+          "Attack on Titan season 3 finished airing last summer and the final season is due to come out this year. So let's do a bit of a recap and unpack the Complete ...",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/dZ_IwkAE2ls/default.jpg",
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/dZ_IwkAE2ls/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/dZ_IwkAE2ls/hqdefault.jpg",
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: "ChannelFrederator",
+        liveBroadcastContent: "none",
+        publishTime: "2019-12-14T16:00:11Z",
+      },
+    },
+    {
+      kind: "youtube#searchResult",
+      etag: "RNupHpTMmjRDURtOppTwmSRGDPI",
+      id: {
+        kind: "youtube#video",
+        videoId: "Ehx2JeeuuMc",
+      },
+      snippet: {
+        publishedAt: "2020-10-13T19:09:56Z",
+        channelId: "UCOsAfA06zghgALnXPp4VbGA",
+        title:
+          "The FLYING TITAN finally revealed?! | Attack on Titan / Shingeki no Kyojin Eren Final Battle Twist!",
+        description:
+          "Support us on Patreon: www.patreon.com/animeuproar The FLYING TITAN finally revealed?! | Attack on Titan / Shingeki no Kyojin Eren Final Battle Twist!",
+        thumbnails: {
+          default: {
+            url: "https://i.ytimg.com/vi/Ehx2JeeuuMc/default.jpg",
+            width: 120,
+            height: 90,
+          },
+          medium: {
+            url: "https://i.ytimg.com/vi/Ehx2JeeuuMc/mqdefault.jpg",
+            width: 320,
+            height: 180,
+          },
+          high: {
+            url: "https://i.ytimg.com/vi/Ehx2JeeuuMc/hqdefault.jpg",
+            width: 480,
+            height: 360,
+          },
+        },
+        channelTitle: "AnimeUproar",
+        liveBroadcastContent: "none",
+        publishTime: "2020-10-13T19:09:56Z",
+      },
+    },
+  ],
+};
