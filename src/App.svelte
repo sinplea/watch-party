ea983843754120b01b9385fdbba45d1da8620d77
Alex Werner <alexanderdwerner@gmail.com>
2020-11-17 11:02:43 -0600

    moves socket io load script to main.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,69 +1,72 @@
-<svelte:head>
-    <meta charset="UTF-8">
-    <meta name="description" content="Watch Party: Where you can YouTube videos together">
-    <meta name="keywords" content="youtube, video sharing, webrtc, websockets">
-    <meta name="author" content="Alex Werner @sinplea">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Mukta&display=swap" rel="stylesheet">
-    <!-- Exposes gapi variable globally -->
-    <script src="https://apis.google.com/js/api.js" on:load={handleGoogleAPI}></script>
-    <script src="https://cdn.socket.io/socket.io-3.0.1.js" on:load={handleSocketIO}></script>
-</svelte:head>
 <script lang="ts">
-    import type { Message } from './lib/Interfaces/ChatInterfaces';
+  import Router from "svelte-spa-router";
+  import MainPage from "./pages/Main.svelte";
+  import NotFoundPage from "./pages/NotFound.svelte";
+  import PartiesPage from "./pages/Parties.svelte";
 
-    import MainPage from './pages/Main.svelte';
+  import type { Message } from "./lib/Interfaces/ChatInterfaces";
 
-    // NOTE: @types/socket.io-client is not installing via npm, so idk, try
-    // to fix later. This works though.
-    let socket: unknown;
-    let incomingMessage: Message;
+  // NOTE: @types/socket.io-client is not installing via npm, so idk, try
+  // to fix later. This works though.
+  let socket: unknown;
+  let incomingMessage: Message;
 
-    import Router from 'svelte-spa-router';
-    import MainPage from './pages/Main.svelte';
-    import NotFoundPage from './pages/NotFound.svelte';
-    import PartiesPage from './pages/Parties.svelte';
-    
-    const routes = {
-      '/': MainPage,
-      '/parties': PartiesPage,
-      '*': NotFoundPage,
-    }
+  const routes = {
+    "/": MainPage,
+    "/parties": PartiesPage,
+    "*": NotFoundPage,
+  };
 
-    /**
-     * Below sets up google api connection for handling youtube queries in later
-     * components. See components/TopBar/TopBarSearch.svelte for example.
-     */
-    function handleGoogleAPI() {
-        // @ts-ignore
-        gapi.load("client", function() {
-            loadClient();
-        });
-    }
+  /**
+   * Below sets up google api connection for handling youtube queries in later
+   * components. See components/TopBar/TopBarSearch.svelte for example.
+   */
+  function handleGoogleAPI() {
+    // @ts-ignore
+    gapi.load("client", function () {
+      loadClient();
+    });
+  }
 
-    function loadClient() {
-        // @ts-ignore
-        gapi.client.setApiKey(API_KEY);
+  function loadClient() {
+    // @ts-ignore
+    gapi.client.setApiKey(API_KEY);
 
-        // @ts-ignore
-        return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-            .then(function() { console.log("GAPI client loaded for API"); },
-            function(err) { console.error("Error loading GAPI client for API", err); });
-    }
-
-    function handleSocketIO() {
-      //@ts-ignore
-      socket = io(LUNA_DEV_SERVER);
-      
-      //@ts-ignore
-      socket.on('message-captured', (message: Message) => {
-        incomingMessage = message;
-      });
-    };
+    // @ts-ignore
+    return gapi.client
+      .load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+      .then(
+        function () {
+          console.log("GAPI client loaded for API");
+        },
+        function (err) {
+          console.error("Error loading GAPI client for API", err);
+        }
+      );
+  }
 </script>
 
+<svelte:head>
+  <meta charset="UTF-8" />
+  <meta
+    name="description"
+    content="Watch Party: Where you can YouTube videos together" />
+  <meta name="keywords" content="youtube, video sharing, webrtc, websockets" />
+  <meta name="author" content="Alex Werner @sinplea" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link
+    href="https://fonts.googleapis.com/icon?family=Material+Icons"
+    rel="stylesheet" />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap"
+    rel="stylesheet" />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Mukta&display=swap"
+    rel="stylesheet" />
+  <!-- Exposes gapi variable globally -->
+  <script src="https://apis.google.com/js/api.js" on:load={handleGoogleAPI}>
+  </script>
+</svelte:head>
 <main>
-    <Router routes={routes}></Router>
+  <Router {routes} />
 </main>
--- src/pages/Main.svelte
+++ src/pages/Main.svelte
@@ -1,75 +1,88 @@
 <script lang="ts">
-    import ChatPanel from '../components/Chat/ChatPanel.svelte';
-    import TopBar from '../components/TopBar/TopBar.svelte';
-    import VideoPlayer from '../components/VideoPlayer/VideoPlayer.svelte';
-    import SideBar from '../components/SideBar/SideBar.svelte';
-    import type { Message } from '../lib/Interfaces/ChatInterfaces';
+  import ChatPanel from "../components/Chat/ChatPanel.svelte";
+  import TopBar from "../components/TopBar/TopBar.svelte";
+  import VideoPlayer from "../components/VideoPlayer/VideoPlayer.svelte";
+  import SideBar from "../components/SideBar/SideBar.svelte";
+  import type { Message } from "../lib/Interfaces/ChatInterfaces";
 
-    export let socket: unknown;
-    export let incomingMessage: Message;
+  export let socket: unknown;
+  let incomingMessage: Message;
 
-    let isSideBarActive: boolean = false;
+  let isSideBarActive: boolean = false;
 
-    function toggleSideBar(e: CustomEvent): void {
-        isSideBarActive = !isSideBarActive;
-    }
+  function toggleSideBar(e: CustomEvent): void {
+    isSideBarActive = !isSideBarActive;
+  }
+
+  function handleSocketIO() {
+    //@ts-ignore
+    socket = io(LUNA_DEV_SERVER);
+
+    //@ts-ignore
+    socket.on("message-captured", (message: Message) => {
+      incomingMessage = message;
+    });
+  }
 </script>
 
-<div class="main-page-wrapper">
-    <TopBar on:toggleSideBar={toggleSideBar}></TopBar>
-    <VideoPlayer></VideoPlayer>
-    <ChatPanel incomingMessage={incomingMessage} socket={socket}></ChatPanel>
-</div>
-
-{#if isSideBarActive}
-    <SideBar on:toggleSideBar={toggleSideBar}></SideBar>
-{/if}
-
-
 <style>
-    .main-page-wrapper {
-        display: grid;
-        width: 100%;
-        background-color: #f0f0f0;
-        color: #434343;
-    }
+  .main-page-wrapper {
+    display: grid;
+    width: 100%;
+    background-color: #f0f0f0;
+    color: #434343;
+  }
 
-    /**
+  /**
         RESPONSIVE BREAKPOINTS
         Based on most common screen sizes of 2019
         https://uxtricks.design/blogs/ux-design/responsive-design/
     */
-    @media (max-width: 375px) {
-        .main-page-wrapper {
-            height: 160vh;
-            grid-template-columns: 100%;
-            grid-template-rows: 8vh 60vh 92vh;
-        }
+  @media (max-width: 375px) {
+    .main-page-wrapper {
+      height: 160vh;
+      grid-template-columns: 100%;
+      grid-template-rows: 8vh 60vh 92vh;
     }
+  }
 
-
-    @media (min-width: 375px) {
-        .main-page-wrapper {
-            height: 160vh;
-            grid-template-columns: 100%;
-            grid-template-rows: 8vh 60vh 92vh;
-        }
+  @media (min-width: 375px) {
+    .main-page-wrapper {
+      height: 160vh;
+      grid-template-columns: 100%;
+      grid-template-rows: 8vh 60vh 92vh;
     }
+  }
 
-    
-    @media (min-width: 768px) {
-        .main-page-wrapper {
-            height: 160vh;
-            grid-template-columns: 100%;
-            grid-template-rows: 8vh 60vh 92vh;
-        }
+  @media (min-width: 768px) {
+    .main-page-wrapper {
+      height: 160vh;
+      grid-template-columns: 100%;
+      grid-template-rows: 8vh 60vh 92vh;
     }
+  }
 
-    @media (min-width: 1366px) {
-        .main-page-wrapper {
-            height: 100vh;
-            grid-template-columns: 80vw auto;
-            grid-template-rows: 8vh 92vh;
-        }
+  @media (min-width: 1366px) {
+    .main-page-wrapper {
+      height: 100vh;
+      grid-template-columns: 80vw auto;
+      grid-template-rows: 8vh 92vh;
     }
+  }
 </style>
+
+<svelte:head>
+  <script
+    src="https://cdn.socket.io/socket.io-3.0.1.js"
+    on:load={handleSocketIO}>
+  </script>
+</svelte:head>
+<div class="main-page-wrapper">
+  <TopBar on:toggleSideBar={toggleSideBar} />
+  <VideoPlayer />
+  <ChatPanel {incomingMessage} {socket} />
+</div>
+
+{#if isSideBarActive}
+  <SideBar on:toggleSideBar={toggleSideBar} />
+{/if}
