425c86723c407a3d591b8c727b014bf505158838
Alex Werner <alexanderdwerner@gmail.com>
2020-11-20 12:56:04 -0600

    Adds blank files that correspond to elements required to build parties page. Design found in screenshots/design

--- src/pages/Parties.svelte
+++ src/pages/Parties.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
+  import TopBar from "../components/TopBar/TopBar.svelte";
 </script>
 
 <style>
+  .parties-page {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: 8vh 12vh 80vh;
+
+    width: 100%;
+    background-color: white;
+    color: #434343;
+  }
 </style>
 
-<div class="parties-page" />
+<div class="parties-page">
+  <TopBar gridColEnd={5} />
+</div>
