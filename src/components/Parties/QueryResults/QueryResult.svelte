1f6b886ddc5e1734b0505e579d5d917dd272ab0e
Alex Werner <alexanderdwerner@gmail.com>
2020-11-25 14:57:44 -0600

    DESIGN: Implements CSS Grid and Element design for QueryResult on Parties Page

--- src/components/Parties/QueryResults/QueryResult.svelte
+++ src/components/Parties/QueryResults/QueryResult.svelte
@@ -6,20 +6,100 @@
 </script>
 
 <style>
+  .thumbnail-container {
+    position: relative;
+    height: 196.88px;
+    width: 350px;
+  }
+
+  .view-count {
+    display: grid;
+    align-items: center;
+    justify-items: center;
+    padding: 1%;
+    border-radius: 1px;
+    grid-template-columns: 1fr 2fr;
+
+    position: absolute;
+    right: 0;
+    top: 0;
+
+    margin-right: 0.5em;
+    margin-top: 0.5em;
+
+    background-color: rgba(0, 0, 0, 72%);
+    color: white;
+  }
+
+  .view-count-icon {
+    grid-column: 1 / 2;
+    font-size: 16px;
+  }
+
+  .view-count-number {
+    grid-column: 2 / 3;
+
+    font-size: 12px;
+    font-weight: 300;
+  }
+
+  .text-content {
+    display: grid;
+    align-items: center;
+    grid-template-rows: repeat(3, 1fr);
+    font-family: "Mukta", sans-serif;
+    color: #434343;
+  }
+
+  .now-watching {
+    height: 1em;
+    grid-row: 1 / 2;
+    margin: 0;
+
+    color: #878787;
+    font-style: italic;
+    font-weight: 300;
+    font-size: 0.8em;
+  }
+
+  .preview-title {
+    height: 1.2em;
+    grid-row: 2 / 3;
+
+    font-size: 1em;
+    font-weight: 400;
+    margin: 0;
+  }
+
+  .preview-topic {
+    height: 1.25em;
+    grid-row: 3 / 4;
+
+    font-size: 0.8em;
+    font-weight: 300;
+    margin: 0;
+  }
 </style>
 
 <div class="query-result" on:click={join}>
   <div class="thumbnail-container">
     <div class="view-count">
-      <span class="material-icons">account_circle</span>
-      <span>{roomListing.watchCount}</span>
+      <span class="view-count-icon material-icons">account_circle</span>
+      <span class="view-count-number">{roomListing.watchCount}</span>
     </div>
-    <div class="thumbnail">
+
+    {#if roomListing.thumbnail !== ''}
       <img src={roomListing.thumbnail} alt="Room thumbnail preview" />
-    </div>
+    {:else}
+      <img
+        src="https://images2.alphacoders.com/942/thumb-1920-942236.jpg"
+        alt="A Sekiro Wallpaper" />
+    {/if}
   </div>
 
-  <div class="now-watching">Now watching</div>
-  <div class="preview-title">{roomListing.title}</div>
-  <div class="preview-topic">{roomListing.topic}</div>
+  <div class="text-content">
+    <div class="now-watching">Now watching</div>
+    <div class="preview-title">{roomListing.title}</div>
+    <div class="preview-topic">{roomListing.topic}</div>
+  </div>
 </div>
--- src/components/Parties/QueryResults/QueryResults.svelte
+++ src/components/Parties/QueryResults/QueryResults.svelte
@@ -5,6 +5,17 @@
   export let results: Array<RoomHTMLElementData>;
 </script>
 
-{#each results as roomListing}
-  <QueryResult {roomListing} />
-{/each}
+<style>
+  .query-results {
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    column-gap: 20px;
+    margin: 0 40px;
+  }
+</style>
+
+<div class="query-results">
+  {#each results as roomListing}
+    <QueryResult {roomListing} />
+  {/each}
+</div>
